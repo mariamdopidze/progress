cc89818db40874047c1438f1ee9befea5351d981
mariamdopidze <119893246+mariamdopidze@users.noreply.github.com>
2023-05-15 14:15:52 +0400

    added files

---
+++ progress/src/Components/notification/Notification.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./notification.css";
+
+function Notification(props){
+    return(
+      <div class={`progress ${props.class}`}>
+      <div class="progress-bar"></div>
+  
+  
+  
+</div>
+    )
+}
+
+export default Notification;
